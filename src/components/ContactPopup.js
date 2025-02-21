3056dc81de92c6a6acb01d1156331c949212097e
Brad Weatherby <bweatherby1@gmail.com git config --global user.name Brad>
2025-02-21 12:11:32 -0600

    updated email and phone to clickable

--- src/components/ContactPopup.js
+++ src/components/ContactPopup.js
@@ -10,9 +10,9 @@ const ContactPopup = ({ isOpen, onClose }) => {
         <h2>Contact Information</h2>
         <div className="contact-info">
           <p><strong>Name:</strong> Brad Weatherby</p>
-          <p><strong>Phone:</strong> (615) 969-9839</p>
-          <p><strong>Email:</strong> bweatherby1@gmail.com</p>
-          <p><strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/brad-weatherby/" target="_blank" rel="noopener noreferrer">linkedin.com/in/brad-weatherby</a></p>
+          <p><strong>Phone:</strong> <a href="tel:+16159699839">(615) 969-9839</a></p>
+          <p><strong>Email:</strong> <a href="mailto:bweatherby1@gmail.com">bweatherby1@gmail.com</a></p>
+          <p><strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/brad-weatherby1/" target="_blank" rel="noopener noreferrer">linkedin.com/in/brad-weatherby</a></p>
           <p><strong>GitHub:</strong> <a href="https://github.com/bweatherby1" target="_blank" rel="noopener noreferrer">github.com/bweatherby1</a></p>
         </div>
         <button className="popup-close-button" onClick={onClose}>Close</button>
